fc9a37510c87f9c83f4005aac75fa56353d518dc
Danilo Lima <danilo.lima@leato.io>
2024-02-06 14:56:21 -0300

    chore: restores B3.import_operations function and removes boilerplate

--- lib/b3.ex
+++ lib/b3.ex
@@ -5,18 +5,14 @@ defmodule B3 do
   alias B3.DTO.OperationDTO
 
   @doc """
-  Hello world.
+  This function is used to transform the file from B3 into a more
+  clean and known format: "ticker;date;time;price;amount".
 
   ## Examples
 
-      iex> B3.hello()
-      :world
-
+      iex> B3.transform_operations("/tmp/25-01-2024_NEGOCIOSAVISTA.txt")
+      :ok
   """
-  def hello do
-    :world
-  end
-
   @spec transform_operations(Path.t()) :: any()
   def transform_operations(file_path) do
     File.stream!(file_path)
@@ -27,12 +23,48 @@ defmodule B3 do
     |> Stream.run()
   end
 
+  @doc """
+  This function uses data from the files created by transform_operations/1
+  and inserts into the database.
+
+  ## Examples
+
+      iex> B3.transform_operations("/tmp/25-01-2024_NEGOCIOSAVISTA.txt.operations.csv")
+      :ok
+  """
+  @spec import_operations(Path.t()) :: any()
+  def import_operations(file_path) do
+    File.stream!(file_path)
+    |> CSV.decode(separator: ?;, headers: true)
+    |> Stream.map(fn {:ok, operation} ->
+      %B3.Models.Operation{}
+      |> B3.Models.Operation.changeset(operation)
+      |> B3.Repo.insert()
+    end)
+    |> Stream.run()
+  end
+
+  @doc """
+  CLI entrypoint for searching operations.
+
+  ## Examples
+
+      iex> B3.find_by_ticker_and_date("INVALID11")
+      No records found for INVALID11 and . Error: %Enum.EmptyError{message: "empty error"}
+
+      iex> B3.find_by_ticker_and_date("SIMH3", "2024-03-29")
+      No records found for INVALID11 and 2024-03-29. Error: %Enum.EmptyError{message: "empty error"}
+
+      iex> B3.find_by_ticker_and_date("WING24", "2024-01-27")
+      %{ticker: "WING24", max_range_value: 129340.0, max_daily_volume: 3}
+
+  """
   @spec find_by_ticker_and_date(any(), any()) :: :ok | B3.OperationResponseDTO.t()
-  def find_by_ticker_and_date(ticker, date) do
+  def find_by_ticker_and_date(ticker, date \\ nil) do
     try do
       B3.Services.Operation.find_by_ticker_and_date(ticker, date)
     rescue
-      e in EmptyError ->
+      e in Enum.EmptyError ->
         IO.puts("No records found for #{ticker} and #{date}. Error: #{inspect(e)}")
     end
   end
--- test/b3_test.exs
+++ test/b3_test.exs
@@ -1,8 +1,4 @@
 defmodule B3Test do
   use ExUnit.Case
   doctest B3
-
-  test "greets the world" do
-    assert B3.hello() == :world
-  end
 end
