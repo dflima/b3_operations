3d463dadf447704aeafe3443bf933f706b0b6297
Danilo Lima <danilo.lima@leato.io>
2024-02-06 00:51:27 -0300

    wip: adds first draft of ticker filter

--- lib/b3/endpoint.ex
+++ lib/b3/endpoint.ex
@@ -14,6 +14,24 @@ defmodule B3.Endpoint do
     send_resp(conn, 200, "pong!")
   end
 
+  get "/operations" do
+    with %{query_params: %{"ticker" => ticker}} <- fetch_query_params(conn) do
+      response = B3.Queries.Operation.find_by_ticker(ticker) |> Enum.map(fn [date, ticker, price, amount] ->
+        %{date: date, ticker: ticker, price: price, amount: amount}
+      end)
+
+      send_resp(conn, 200, Jason.encode!(response))
+    else
+      _ -> send_resp(conn, 400, "bad request")
+    end
+
+    # {
+    #   ticker: "PETR4",
+    #   max_range_value: 0,
+    #   max_daily_volume: 0
+    # }
+  end
+
   match _ do
     send_resp(conn, 404, "Route not found.")
   end
