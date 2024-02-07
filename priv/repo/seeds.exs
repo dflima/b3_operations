cb86a5e1f278e03869f9e91ac0ecc01a042dc10c
Danilo Lima <danilo.lima@leato.io>
2024-02-07 17:50:11 -0300

    chore(tests): fixes seed file

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,40 +1,35 @@
 
-B3.Repo.insert!(%B3.Models.Operation{
+B3.Repo.insert!(%B3.Models.Projection{
   ticker: "WING24",
   price: 129210000,
   amount: 5,
-  time: ~T[10:38:29],
   date: ~D[2024-01-25]
 })
 
-B3.Repo.insert!(%B3.Models.Operation{
+B3.Repo.insert!(%B3.Models.Projection{
   ticker: "WING24",
   price: 129210000,
   amount: 5,
-  time: ~T[10:38:29],
   date: ~D[2024-01-26]
 })
 
-B3.Repo.insert!(%B3.Models.Operation{
+B3.Repo.insert!(%B3.Models.Projection{
   ticker: "WING24",
   price: 129210000,
   amount: 7,
-  time: ~T[10:38:29],
   date: ~D[2024-01-26]
 })
 
-B3.Repo.insert!(%B3.Models.Operation{
+B3.Repo.insert!(%B3.Models.Projection{
   ticker: "WING24",
   price: 129340000,
   amount: 3,
-  time: ~T[10:38:29],
   date: ~D[2024-01-27]
 })
 
-B3.Repo.insert!(%B3.Models.Operation{
+B3.Repo.insert!(%B3.Models.Projection{
   ticker: "SIMH3",
   price: 7970,
   amount: 900,
-  time: ~T[10:38:29],
   date: ~D[2024-01-25]
 })
